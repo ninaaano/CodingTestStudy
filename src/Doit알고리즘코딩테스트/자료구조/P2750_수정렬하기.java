bf805d85155f563dd71d947bf65fde46d6b9e2f2
Life is a ninanino <tvxqinak@naver.com>
2023-01-08 20:47:05 +0900

    Create P2750_수정렬하기.java

---
+++ src/Doit알고리즘코딩테스트/자료구조/P2750_수정렬하기.java
@@ -0,0 +1,33 @@
+package Doit알고리즘코딩테스트.자료구조;// @ author ninaaano
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P2750_수정렬하기 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * 버블정렬을 사용해서 풀어보기
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int x : arr) {
+            System.out.println(x);
+        }
+        br.close();
+    }
+}
