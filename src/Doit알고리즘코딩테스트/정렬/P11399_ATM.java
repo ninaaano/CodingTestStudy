380c51a35b19f2aaef2f77688e2c35561af5c215
Life is a ninanino <tvxqinak@naver.com>
2023-01-12 02:10:55 +0900

    Create P11399_ATM.java

---
+++ src/Doit알고리즘코딩테스트/정렬/P11399_ATM.java
@@ -0,0 +1,48 @@
+package Doit알고리즘코딩테스트.정렬;// @ author ninaaano
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class atmQueue implements Comparable<atmQueue> {
+    int index; // 대기번호
+    int time; // 대기 시간
+
+    public atmQueue(int index, int time) {
+        this.index = index;
+        this.time = time;
+    }
+
+
+    @Override
+    public int compareTo(atmQueue o) {
+        return this.time - o.time; // 시간 오름차순
+    }
+}
+
+
+public class P11399_ATM {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        atmQueue[] arr = new atmQueue[n];
+
+        for(int i =0; i<n; i++){
+            arr[i] = new atmQueue(i, Integer.parseInt(st.nextToken()));
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        int total = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i].time;
+            total += sum;
+        }
+        System.out.println(total);
+    }
+}
