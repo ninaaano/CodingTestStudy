09181a1301e860311ab91985268a0af307498000
Life is a ninanino <tvxqinak@naver.com>
2023-02-07 02:23:12 +0900

    유클리드호제법

---
+++ src/Doit알고리즘코딩테스트/유클리드호제법/P1934_최소공배수.java
@@ -0,0 +1,37 @@
+package Doit알고리즘코딩테스트.유클리드호제법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1934_최소공배수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int d = gcd(a,b);
+
+            sb.append(a*b/d).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static int gcd(int a, int b) {
+
+        while(b!=0){
+            int r = a%b;
+            a=b;
+            b=r;
+        }
+        return a;
+    }
+}
